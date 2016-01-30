f59c2aacb790940eb87c8862d759d01861bed4a7
David Stroud <david@dwstroud.com>
2016-01-30 12:35:06 +0000

    Added main local files

--- 01-SuperCool/ViewController.swift
+++ 01-SuperCool/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redBomb: UIImageView!
+    @IBOutlet weak var blackBomb: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideRed(sender: AnyObject) {
+        redBomb.hidden = true
+    }
+    
+    @IBAction func hideBlack(sender: AnyObject) {
+        blackBomb.hidden = true
+    }
+    
+    @IBAction func toggleRedBomb(sender: AnyObject) {
+        if (redBomb.hidden == false) {
+            redBomb.hidden = true
+        } else {
+            redBomb.hidden = false
+        }
+    }
+
+    @IBAction func toggleBlackBomb(sender: AnyObject) {
+        if (blackBomb.hidden == false) {
+            blackBomb.hidden = true
+        } else {
+            blackBomb.hidden = false
+        }
+    }
 
 }
 
